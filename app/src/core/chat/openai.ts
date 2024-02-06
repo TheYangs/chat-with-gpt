27cd175cb4800b2ea7a52ed62f2b29d60a07a060
Jay Yang <j@theyangs.us>
2024-02-06 13:32:40 -0600

    fix the dups

--- app/src/core/chat/openai.ts
+++ app/src/core/chat/openai.ts
@@ -147,7 +147,6 @@ export const maxTokensByModel = {
     "gpt-4-turbo-preview": 128000,
     "gpt-4": 8192,
     "gpt-4-0613": 8192,
-    "gpt-4-1106-preview": 4096,
     "gpt-4-32k": 32768,
     "gpt-4-32k-0613": 32768,
     "gpt-3.5-turbo-0613": 4096,
